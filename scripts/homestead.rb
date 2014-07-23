4d0afa0d46e7d10774f6378522a06ff75e8555f6
Hugo Leonardo Costa e Silva <hugoleodev@gmail.com>
2014-07-23 12:39:43 -0300

    Alterando a box padrao para a do melhor menu e adcionando a porta do elasticsearch

--- scripts/homestead.rb
+++ scripts/homestead.rb
@@ -1,7 +1,7 @@
 class Homestead
   def Homestead.configure(config, settings)
     # Configure The Box
-    config.vm.box = "laravel/homestead"
+    config.vm.box = "melhormenu/mmdev"
     config.vm.hostname = "homestead"
 
     # Configure A Private Network IP
@@ -18,7 +18,7 @@ class Homestead
     # Configure Port Forwarding To The Box
     config.vm.network "forwarded_port", guest: 80, host: 8000
     config.vm.network "forwarded_port", guest: 3306, host: 33060
-    config.vm.network "forwarded_port", guest: 5432, host: 54320
+    config.vm.network "forwarded_port", guest: 9200, host: 9200
 
     # Configure The Public Key For SSH Access
     config.vm.provision "shell" do |s|
